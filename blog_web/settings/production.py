68fa39cd29809c56c897da2f8eae8671003eb60c
unknown <gregoriazuaje04@gmail.com>
2022-03-08 11:32:54 -0400

    cambios hecho para establecer parametros para producción

--- blog_web/settings/production.py
+++ blog_web/settings/production.py
@@ -1,4 +1,10 @@
 from .base import *
+from os import getenv
+import django_heroku
+
+SECRET_KEY = getenv('SECRET_KEY')
+
+DEBUG = False
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
@@ -7,3 +13,5 @@ MEDIA_URL = '/uploaded-images/'
 STATIC_URL = '/static-files/'
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+django_heroku.settings(locals())
