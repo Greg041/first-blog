18fbbb179bb9675f9c95974b0c72200e19f3ed5e
unknown <gregoriazuaje04@gmail.com>
2022-03-08 21:17:20 -0400

    updated settings in productions to serve media files

--- blog_web/settings/production.py
+++ blog_web/settings/production.py
@@ -1,15 +1,25 @@
 from os import getenv
 from .base import *
 import django_heroku
+import cloudinary
+import cloudinary_storage
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+django_heroku.settings(locals())
+
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
 MEDIA_URL = '/uploaded-images/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUD_NAME'),
+    'API_KEY': os.getenv('API_KEY'),
+    'API_SECRET': os.getenv('API_SECRET'),
+}
 
-django_heroku.settings(locals())
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
