d205c2bbc29734e629b091f54d43cdd75c5b2097
Gregori <gregoriazuaje04@gmail.com>
2024-09-14 22:14:37 -0400

    Agregadas librerias y funcionalidades para deployment a producción

--- blog_web/settings/production.py
+++ blog_web/settings/production.py
@@ -1,16 +1,23 @@
 import os
 from .base import *
 import cloudinary
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+local_env_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+    
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 MEDIA_URL = '/uploaded-images/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 cloudinary.config(
     cloud_name = os.getenv('CLOUD_NAME'),
